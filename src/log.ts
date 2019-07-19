dc458ac8da5e483b859c562cadc10f1b8cd4a8ef
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2019-07-19 12:56:28 +0700

    Log to stderr instead of stdout

--- src/log.ts
+++ src/log.ts
@@ -1,14 +1,17 @@
 import pino from 'pino'
 
-const rootLogger = pino({
-  prettyPrint:
-    (process.env.NODE_ENV || 'development') === 'development'
-      ? { translateTime: true }
-      : false,
-})
+const rootLogger = pino(
+  {
+    prettyPrint:
+      (process.env.NODE_ENV || 'development') === 'development'
+        ? { translateTime: true }
+        : false,
+  },
+  pino.destination(2),
+)
 
 export default rootLogger
 
 export function logger(moduleName: string) {
-  return rootLogger.child({ module: moduleName })
+  return rootLogger.child({ name: moduleName })
 }
